6e7931d960452bc14a097f2656177b2f00d4c1ae
guitar7531 <guitar7531@gmail.com>
2016-02-20 13:31:00 +0300

    Added files via upload

---
+++ task1/main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+
+int main(int args, char *argv)
+{
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "o", stdout);
+
+	int n;
+	scanf("%d", &n);
+	int **a;
+	a = malloc(sizeof(int)*n);
+	for (int i = 0; i < n; i++)
+		a[i] = malloc(sizeof(int)* n);
+	for (int i = 0; i < n; i++)
+	for (int j = 0; j < n; j++)
+		a[i][j] = (i + 1)*(j + 1);
+	int x1, x2, y1, y2;
+	while (true){
+		scanf("%d", &x1);
+		if (x1 == 0)return 0;
+		scanf("%d%d%d",&y1,&x2,&y2);
+		for (int i = y1 - 1; i <= y2 - 1; i++)
+		{
+			for (int j = x1 - 1; j <= x2 - 1; j++)
+				printf("%d ", a[i][j]);
+			printf("\n");
+		}
+	}
+}
