406bd961b8e686608238d660535ed48ba723636a
guitar7531 <guitar7531@gmail.com>
2016-05-18 00:32:15 +0300

    Update rational.cpp

--- task3/rational.cpp
+++ task3/rational.cpp
@@ -1,28 +1,27 @@
 #include "rational.h"
 
 
-rational::rational(int x){
+rational::rational(int x) {
     n = x;
     d = 1;
 }
 
-rational::rational(int x, int y){
+rational::rational(int x, int y) {
     n = x;
     d = y;
     simplify(n, d);
 }
 
-int rational::getNum() const{
+int rational::getNum() const {
     return n;
 }
 
-int rational::getDenom() const{
+int rational::getDenom() const {
     return d;
 }
 
-int gcd(int a, int b){
-    for (;;)
-    {
+int gcd(int a, int b) {
+    for (; ;) {
         if (a == 0) return b;
         b %= a;
         if (b == 0) return a;
@@ -30,34 +29,37 @@ int gcd(int a, int b){
     }
 }
 
-int lcm(int a, int b){
+int lcm(int a, int b) {
     int temp = gcd(a, b);
 
     return temp ? (a / temp * b) : 0;
 }
 
-rational rational::operator +(rational const &x) const {
+rational rational::operator+(rational const &x) const {
     int denom = lcm(d, x.d);
     int num = (n * (denom / d)) + (x.n * (denom / x.d));
     return rational(num, denom);
 }
 
-rational rational::operator -(rational const &x) const {
+rational rational::operator-(rational const &x) const {
     int denom = lcm(d, x.d);
     int num = (n * (denom / d)) - (x.n * (denom / x.d));
     return rational(num, denom);
 }
 
-rational rational::operator *(rational const &x) const {
+rational rational::operator*(rational const &x) const {
     return rational(n * x.n, d * x.d);
 }
 
-rational rational::operator /(rational const &x) const {
+rational rational::operator/(rational const &x) const {
     return rational(n * x.d, d * x.n);
 }
 
-void rational::simplify(int &a, int &b){
+void rational::simplify(int &a, int &b) {
     int c = gcd(a, b);
-    a /= c;
-    b /= c;
+    while (c != 1) {
+        a /= c;
+        b /= c;
+        c = gcd(a, b);
+    }
 }
