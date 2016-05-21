e2b56ff445f9d09ce060db571f103c84d7895a72
guitar7531 <guitar7531@gmail.com>
2016-05-21 13:08:18 +0300

    Update lazy_string.h

--- task6/lazy_string.h
+++ task6/lazy_string.h
@@ -17,15 +17,33 @@ class lazy_string{
         lazy_string *s;
     };
 public:
+
+    //Constructor of new lazy_string from std::string
     lazy_string(const string &);
+    
+    //Returns the number of characters in the lazy_string
     size_t size() const;
+    
+    //Returns the number of characters in the lazy_string
     size_t lenght() const;
+    
+    //Returns the referens to the character which is located on the position "pos"
     char at(size_t) const;
-    char operator[](size_t) const;
-    lazy_string substr(size_t, size_t);
-    friend istream &operator>>(istream &, lazy_string &);
-    friend ostream &operator<<(ostream &, lazy_string &);
+    
+    //Returns the referens to the character which is located on the position "pos"
     chr operator[](size_t);
+    char operator[](size_t) const;
+    
+    //Receivs position of first symbol and the number of characters and
+    //returns a substring which is int the interval [position, position+number)
+    lazy_string substr(size_t, size_t);
+    
+    //Reads a string from the input stream 
+    friend istream &operator>>(istream &, lazy_string &);
+    
+    //Writes a string in the output stream
+    friend ostream &operator<<(ostream &, lazy_string &);
+    
 private:
     size_t bg, sz;
     shared_ptr<string> r;
